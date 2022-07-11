5ae29fc0e42f2e7645954326bb2d97aa70c10e83
itzganesh <modiganesh83@gmail.com>
2022-07-11 23:47:42 +0530

    add initial components

---
+++ src/components/Announce.jsx
@@ -0,0 +1,13 @@
+import { Close } from '@material-ui/icons'
+import React from 'react'
+
+function Announce() {
+  return (
+    <div>
+        <h2> Announcement</h2>
+        <Close/>
+    </div>
+  )
+}
+
+export default Announce
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Announce from '../components/Announce'
+
+function Home() {
+  return (
+    <div>
+      Home page
+      <Announce/>
+    </div>
+  )
+}
+
+export default Home
+
